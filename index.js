72e7ffc5d4d87f8e4c28ab64e11a7029acd50d86
VIkions <99107287+vikions@users.noreply.github.com>
2025-06-09 21:42:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 let guesses = {}; // В реальности — заменить на БД
 
